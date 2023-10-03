0fc90d3952238f9cb0546d40daa6935aaf6174ed
lorkorblaq <f3mioloko@gmail.com>
2023-10-03 05:08:41 +0100

    update

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -15,11 +15,11 @@ exec {'Hello':
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/lorkorblaq.tech\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
 exec {'run':
   command  => 'sudo service nginx restart',
   provider => shell,
-}
+} 
