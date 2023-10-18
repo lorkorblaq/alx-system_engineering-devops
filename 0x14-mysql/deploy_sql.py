1b523e721516704289a6b4e3314bf1e1c530a94d
blaq <f3mioloko@gmail.com>
2023-10-18 14:23:47 +0100

    update

--- 0x14-mysql/deploy_sql.py
+++ 0x14-mysql/deploy_sql.py
@@ -2,23 +2,30 @@
 from fabric.api import *
 
 env.use_ssh_config = True
-env.hosts = [ 'blaq2']
+env.hosts = [  'blaq2']
 
 
 def install_mysql():
     """
     Install MySQL on a remote server if not already installed
     """
-    # Check if MySQL is already installed
-    # Update the package list and install MySQL server
+
+    #removes previous installed files
+    sudo('rm -rf /etc/mysql /var/lib/mysql')
+
+    sudo('apt-get remove --purge mysql-server mysql-client mysql-common mysql-server-core-* mysql-client-core-*')
+    sudo('apt-get autoremove')
+    sudo('apt-get autoclean')  
+    sudo('apt-get -f install')
+
+    put("signature.key", '/tmp/')
+    #make sure you saved your signatue.key on same directory as fabfile.py
+    sudo('apt-key add /tmp/signature.key')
+    sudo("""sudo sh -c 'echo "deb http://repo.mysql.com/apt/ubuntu bionic mysql-5.7" >> /etc/apt/sources.list.d/mysql.list'""")
+
     sudo('apt-get update')
-    sudo('apt-get install -y mysql-server')
-    #Start the MySQL service
-    sudo('systemctl start mysql')
-    #Enable MySQL to start on boot
-    sudo('systemctl enable mysql')
-    # Secure the MySQL installation (set rootpassword, remove anonymous users, etc.)
-    sudo('mysql_secure_installation')
-    print("MySQL installation complete!")
-    sudo('systemctl status mysql')
+    sudo("sudo apt install -f mysql-client=5.7* mysql-community-server=5.7* mysql-server=5.7*")
+
+    
+
 
