4a26de3df9eda59055b1d757ec1bb4b7601b09f1
blaq <f3mioloko@gmail.com>
2023-10-18 12:02:06 +0100

    update

---
+++ 0x13-firewall/0-block_all_incoming_traffic_but.py
@@ -0,0 +1,23 @@
+from fabric.api import *
+
+env.use_ssh_config = True
+env.hosts = ['blaq1', 'blaq2', 'loadb']
+
+def ufw():
+    """
+        Setup firewall
+    """
+    # sudo("apt-get -y update")
+    # sudo("apt-get -y install ufw")
+    sudo("ufw disable")
+    sudo("ufw default deny incoming")
+    sudo("ufw default allow outgoing")
+    sudo("ufw disable")
+    sudo("ufw allow 22/tcp")
+    sudo("ufw allow 443/tcp")
+    sudo("ufw allow 80/tcp")
+    sudo("ufw enable")
+    sudo("ufw reload")
+    # Display ufw status
+    sudo("ufw status")
+
